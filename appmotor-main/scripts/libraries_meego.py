2742f87b2899cf65eafe00d95b9c52cfcc78dbb1
MQ-HZY-For-Cute <77338936+MQ-HZY-For-Cute@users.noreply.github.com>
2022-05-30 13:25:52 +0800

    Delete appmotor-main directory

--- appmotor-main/scripts/libraries_meego.py
+++
@@ -1,7 +0,0 @@
-# Library statuses
-D = 0   # dlopen
-L = 1   # link
-
-libraries = [
-    (D, "/usr/lib/libmeegoqmllauncher.so.0",                                "-lmeegoqmllauncher", "libmeegoqmllauncher", "meego-qml-launcher-devel"),
-]
--- appmotor-main/src/launcherlib/launcherlib.h
+++
@@ -1,6 +0,0 @@
-#ifndef LAUNCHERLIB_H
-#define LAUNCHERLIB_H
-
-#define DECL_EXPORT __attribute__ ((__visibility__("default")))
-
-#endif
